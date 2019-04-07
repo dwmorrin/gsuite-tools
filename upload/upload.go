b4146d65fcb396d47cd2dead5053705d0d5e3156
David Morrin <dwmorrin@gmail.com>
2019-04-07 17:37:26 -0400

    fix: makes file name and type automatic
    also adds option to specify api secret file path

--- upload/upload.go
+++ upload/upload.go
@@ -1,54 +1,58 @@
 // upload is a general command line tool for Google Drive
 // currently a work in progress
-// current goal: make a very general wrapper for the service.Files.Create call.
-// worked OK to upload a .zip in root personal Drive
-// TODO first test uploaded a file named "untitled" - match current file name to Drive title
-// TODO add ability to target folder (should allow team drive folders as well)
-// TODO look for or write a helper function or helper package to automatically determine
-//        the appropriate mime type based on the given local file path given. (currently hardcoded to .zip)
+// current goal: make a general wrapper for the service.Files.Create call.
+// TODO add target folder option
+// TODO add team drive option
+
 package main
 
 import (
+    "flag"
     "fmt"
     "log"
     "github.com/dwmorrin/gsuite-tools/auth"
     "google.golang.org/api/drive/v3"
     "os"
+    "os/user"
+    "path/filepath"
 )
 
-// command line arguments
-const (
-    programName = iota
-    secretPath
-    sourceData
-)
-const minArgLength = sourceData + 1
+// global default TODO move to config file
+var defaultSecretPath = filepath.Join(".credentials", "secret", "upload.json")
 
 func main() {
-    if len(os.Args) != minArgLength {
-        fmt.Printf("Usage: %v secretPath sourceDataPath (not yet:) targetSheetId" +
-            "\n  secretPath        google cloud crenditials file path" +
-            "\n  sourceDataPath    file you want to upload (hardcoded for zip)" +
-            "\n  (not implemented yet) targetFolderId    target parent folder\n",
-            os.Args[programName])
+    var secretPath string
+    flag.StringVar(&secretPath, "s", getDefaultSecretPath(),
+        "google cloud crenditials file path",
+    )
+    flag.Parse()
+    if flag.Arg(0) == "" {
+        //fmt.Printf("Usage: %v secretPath sourceDataPath (not yet:) targetSheetId" +
+        //    "\n  secretPath        google cloud crenditials file path" +
+        //    "\n  sourceDataPath    file you want to upload (hardcoded for zip)" +
+        //    "\n  (not implemented yet) targetFolderId    target parent folder\n",
+        //    os.Args[programName])
+        fmt.Println("Usage:", os.Args[0], "[options] file")
+        flag.PrintDefaults()
         os.Exit(1)
     }
 
     // Get Drive Service
-    client := auth.GetClient(os.Args[secretPath], drive.DriveScope)
+    client := auth.GetClient(secretPath, drive.DriveScope)
     srv, err := drive.New(client)
     if err != nil {
         log.Fatalf("Unable to retrieve drive Client %v", err)
     }
 
-    newData, err := os.Open(os.Args[sourceData])
+    newData, err := os.Open(flag.Arg(0))
     if err != nil {
         log.Fatalf("Unable to open data file %v", err)
     }
     defer newData.Close()
 
-    newFile := drive.File{}
-    newFile.MimeType = `application/zip` // TODO determine type based on user input
+    newFile := drive.File{
+        Name: filepath.Base(newData.Name()),
+    }
 
     // make the update call
     _, err = srv.Files.Create(&newFile).
@@ -58,4 +62,13 @@ func main() {
     if err != nil {
         log.Fatalf("Unable to upload data %v", err)
     }
+}
+
+func getDefaultSecretPath() string {
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+    path := filepath.Join(user.HomeDir, defaultSecretPath)
+    return  path
 }
